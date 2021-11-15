cc741fa913d753a574ba6199431f498ce47ebc03
airtoum <aidanlesh@gmail.com>
2021-11-14 21:20:36 -0500

    EventPrinter

--- BoardGameMishMash/Debug/EventPrinter.gd
+++ BoardGameMishMash/Debug/EventPrinter.gd
@@ -9,6 +9,12 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	GameEvents.connect("game_state_switched", self, "game_state_switched")
+	GameEvents.connect("game_state_selecting_piece", self, "game_state_selecting_piece")
+	GameEvents.connect("game_state_selecting_space", self, "game_state_selecting_space")
+	GameEvents.connect("game_state_selecting_number", self, "game_state_selecting_number")
+	GameEvents.connect("game_state_animation", self, "game_state_animation")
+	GameEvents.connect("piece_selected", self, "piece_selected")
+	GameEvents.connect("highlight_spaces", self, "highlight_spaces")
 
 
 func game_state_switched(new_game_state):
@@ -25,6 +31,9 @@ func game_state_selecting_number():
 	
 func game_state_animation():
 	print("Event: game_state_animation")
+	
+func piece_selected(piece):
+	print("Event: piece_selected " + str(piece))
 	
 func highlight_spaces(position_array, moving_piece):
 	print("Event: highlight_spaces in positions " + str(position_array) + " for piece " + str(moving_piece))
