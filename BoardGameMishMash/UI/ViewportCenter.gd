abab6b19e9c8eaed8adcb661476db4ff9b30b7a6
airtoum <aidanlesh@gmail.com>
2021-12-05 22:42:58 -0500

    Viewport resizes very nicely

---
+++ BoardGameMishMash/UI/ViewportCenter.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	get_tree().get_root().connect("size_changed", self, "resized")
+
+
+func resized():
+	OS.window_size
